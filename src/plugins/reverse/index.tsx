ab691f102aea088742bc78aa51411d6437a7922c
ToastedToast <hey@toasted.dev>
2024-04-01 18:52:22 +0800

    feat(reverse-plugin): button

--- src/plugins/reverse/index.tsx
+++ src/plugins/reverse/index.tsx
@@ -3,9 +3,10 @@
 import { ChatBarButton, addChatBarButton } from "@api/ChatButtons";
 import { NavContextMenuPatchCallback } from "@api/ContextMenu";
 import definePlugin from "@utils/types";
-import { Menu } from "@webpack/common";
+import { ChannelStore, Menu } from "@webpack/common";
 import { settings } from "./settings";
 import { addPreSendListener, removePreSendListener } from "@api/MessageEvents";
+import { addButton } from "@api/MessagePopover";
 
 const reverseMessage = (content: string) =>
     content.split("").reverse().join("");
@@ -48,6 +49,34 @@ export default definePlugin({
             if (!message.content) return;
 
             message.content = reverseMessage(message.content);
+        });
+        addButton("vc-reverse-message", (message) => {
+            if (!message.content) return null;
+
+            return {
+                label: "Reverse Message",
+                icon: () => (
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                    >
+                        <path d="M9 14 4 9l5-5" />
+                        <path d="M4 9h10.5a5.5 5.5 0 0 1 5.5 5.5v0a5.5 5.5 0 0 1-5.5 5.5H11" />
+                    </svg>
+                ),
+                message,
+                channel: ChannelStore.getChannel(message.channel_id),
+                onClick: () => {
+                    message.content = reverseMessage(message.content);
+                },
+            };
         });
         console.log("If you're reading this, the Reverse plugin loaded");
     },
