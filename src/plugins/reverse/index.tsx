e7f4ea9b5bd0321f93e9e9f3f03116290b7fbf40
ToastedToast <hey@toasted.dev>
2024-04-01 19:01:59 +0800

    feat(reverse-plugin): reverse chat button

--- src/plugins/reverse/index.tsx
+++ src/plugins/reverse/index.tsx
@@ -1,12 +1,20 @@
 /* eslint-disable simple-header/header */
 
-import { ChatBarButton, addChatBarButton } from "@api/ChatButtons";
+import "./styles.css";
+
+import {
+    addChatBarButton,
+    ChatBarButton,
+    removeChatBarButton,
+} from "@api/ChatButtons";
 import { NavContextMenuPatchCallback } from "@api/ContextMenu";
+import { addPreSendListener, removePreSendListener } from "@api/MessageEvents";
+import { addButton, removeButton } from "@api/MessagePopover";
+import { classNameFactory } from "@api/Styles";
 import definePlugin from "@utils/types";
 import { ChannelStore, Menu } from "@webpack/common";
+
 import { settings } from "./settings";
-import { addPreSendListener, removePreSendListener } from "@api/MessageEvents";
-import { addButton } from "@api/MessagePopover";
 
 const reverseMessage = (content: string) =>
     content.split("").reverse().join("");
@@ -25,6 +33,51 @@ const patchMessageContextMenu: NavContextMenuPatchCallback = (
                 message.content = reverseMessage(message.content);
             }}
         />,
+    );
+};
+
+function ReverseIcon({ className }: { className: string }) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className={className}
+        >
+            <path d="M9 14 4 9l5-5" />
+            <path d="M4 9h10.5a5.5 5.5 0 0 1 5.5 5.5v0a5.5 5.5 0 0 1-5.5 5.5H11" />
+        </svg>
+    );
+}
+
+export const cl = classNameFactory("vc-reverse-");
+
+const ReverseChatBarIcon: ChatBarButton = ({ isMainChat }) => {
+    const { autoReverse } = settings.use(["autoReverse"]);
+
+    if (!isMainChat) return null;
+
+    const toggle = () => {
+        const newState = !autoReverse;
+        settings.store.autoReverse = newState;
+    };
+
+    return (
+        <ChatBarButton
+            tooltip="Toggle Reverse Message"
+            onClick={() => toggle()}
+            buttonProps={{
+                "aria-haspopup": "dialog",
+            }}
+        >
+            <ReverseIcon className={cl({ "auto-reverse": autoReverse })} />
+        </ChatBarButton>
     );
 };
 
@@ -50,27 +103,13 @@ export default definePlugin({
 
             message.content = reverseMessage(message.content);
         });
+        addChatBarButton("vc-reverse", ReverseChatBarIcon);
         addButton("vc-reverse-message", (message) => {
             if (!message.content) return null;
 
             return {
                 label: "Reverse Message",
-                icon: () => (
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    >
-                        <path d="M9 14 4 9l5-5" />
-                        <path d="M4 9h10.5a5.5 5.5 0 0 1 5.5 5.5v0a5.5 5.5 0 0 1-5.5 5.5H11" />
-                    </svg>
-                ),
+                icon: ReverseIcon,
                 message,
                 channel: ChannelStore.getChannel(message.channel_id),
                 onClick: () => {
@@ -82,5 +121,7 @@ export default definePlugin({
     },
     stop() {
         removePreSendListener(this.preSend);
+        removeChatBarButton("vc-reverse");
+        removeButton("vc-reverse-message");
     },
 });
